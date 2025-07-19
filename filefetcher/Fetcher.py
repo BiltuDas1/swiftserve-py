5cec371e0649b62136d3c5c054c8fc9a3db6ece8
BiltuDas1 <billionto@gmail.com>
2025-07-19 13:05:38 +0530

    Remove the part file

--- filefetcher/Fetcher.py
+++ filefetcher/Fetcher.py
@@ -33,23 +33,11 @@ class Fetcher:
     """
     while len(self.__queue) != 0:
       work = self.__queue.popleft()
-      file_path: str = os.path.join(
-          Env.get("DOWNLOADS"), f"{work.filename}.part"
-      )
       destination_path: str = os.path.join(Env.get("DOWNLOADS"), work.filename)
       filelist: FileList.FileList = Env.get("FILES")
 
       # Check if the whole file is downloaded
       if filelist.isDownloaded(work.filename):
-        continue
-
-      # Fetching how much downloaded the current file
-      downloaded = 0
-      if os.path.exists(file_path):
-        downloaded = os.path.getsize(file_path)
-
-      # If the chunk download is complete
-      if downloaded >= (work.end_byte - work.start_byte):
         continue
 
       not_next_chunk = False
@@ -66,47 +54,35 @@ class Fetcher:
           except Exception:
             continue
 
-          if response.status_code in (200, 206):
-            mode = "ab" if downloaded > 0 else "wb"
-            with open(file_path, mode) as f:
-              f.write(response.content)
-          else:
+          # If hash doesn't match, then retry downloading
+          sha1 = hashlib.sha1(response.content).hexdigest()
+          if sha1 != work.sha1:
             continue
 
-          # Appending the chunk at the end of the file, when the sha1 hash matched
-          with open(file_path, "rb") as readF:
-            sha1 = hashlib.sha1(readF.read()).hexdigest()
-            # If the file chunk is valid
-            if sha1 == work.sha1:
-              # If not downloaded the exact next chunk, then take the work and add it to the end of the work queue
-              if not filelist.completed(work.filename, work.chunk):
-                self.__queue.append(work)
-                not_next_chunk = True
-                break
+          if response.status_code not in (200, 206):
+            continue
 
-              filelist_path = Env.get("FILELIST_PATH")
-              filelist.save(filelist_path)
-              readF.seek(0)
-              with open(destination_path, 'ab') as writeF:
-                writeF.write(readF.read())
-              break
+          # If not downloaded the exact next chunk, then take the work and add it to the end of the work queue
+          if not filelist.completed(work.filename, work.chunk):
+            self.__queue.append(work)
+            not_next_chunk = True
+            break
 
-          # Delete the chunk
-          if os.path.exists(file_path):
-            os.remove(file_path)
+          filelist_path = Env.get("FILELIST_PATH")
+          filelist.save(filelist_path)
+          # Appending the at the end of the file
+          with open(destination_path, 'ab') as writeF:
+            writeF.write(response.content)
+          break
       else:
-        print(f"invalid file chunk: {file_path}")
+        print(f"invalid chunk {work.chunk}")
         destination_path: str = os.path.join(
             Env.get("DOWNLOADS"), work.filename
         )
         with open(Env.get("LOGFILE"), "a") as f:
           f.write(
-              f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] failed to download `{work.filename}.{work.chunk}.part` of `{destination_path}`\n"
+              f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] failed to download chunk {work.chunk} of `{destination_path}`\n"
           )
-
-        # Delete the incomplete chunk (If exist)
-        if os.path.exists(file_path):
-          os.remove(file_path)
         continue
 
       # If the current chunk is not next chunk, then skip task
@@ -161,10 +137,6 @@ class Fetcher:
 
       # If the chunk is the last part of the file then check if the whole file is intact
       if work.chunk == work.total_chunks:
-        # Delete the .part file
-        if os.path.exists(file_path):
-          os.remove(file_path)
-
         with open(destination_path, 'rb') as f:
           sha512 = hashlib.sha512(f.read()).hexdigest()
           fileinfo = filelist.get(work.filename)
